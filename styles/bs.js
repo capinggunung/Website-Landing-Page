b640952f4314aafcbf39d34c9b45b546d638269a
Agus Waluyo <54011058+capinggunung@users.noreply.github.com>
2025-08-26 22:24:53 +0700

    upload js dan css sebagai style web

---
+++ styles/bs.js
@@ -0,0 +1,9 @@
+function aksi(){
+	document.getElementById('potoprofil').style.width = "500px"  ;
+				}
+function balik() {
+	document.getElementById("potoprofil").style.width = "200px"  ;
+				}
+function klik() {
+  document.getElementsByClassName("a").style.color = "blue";
+}
